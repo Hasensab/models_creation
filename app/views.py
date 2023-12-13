140a7bd40337623dd1e7c1b93090d0a5eb951ddb
Hasensab <r161135@rguktrkv.ac.in>
2023-12-13 18:42:05 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from app.models import *
 
 # Create your views here.
+def display_topic(request):
+    DLTO=Topic.objects.all()
+    d={'topics':DLTO}
+    return render(request,'display_topic.html',context=d)
+def display_webpage(request):
+    DLWO=Webpage.objects.all()
+    d={'webpages':DLWO}
+    return render(request,'display_webpages.html',context=d)
+
+def display_accessrecord(request):
+    DLAO=AccessRecord.objects.all()
+    d={'accessrecord':DLAO}
+    return render(request,'display_accessrecord.html',context=d)
